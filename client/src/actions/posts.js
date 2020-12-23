50950d7067d00da2f11e609fd17220fd5eef2ef9
mani3354488 <mani3354488@gmail.com>
2020-12-23 20:01:16 +0530

    save

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,13 @@
 import * as api from '../api';
 
-//action creators
+//action creators are functions that return an action
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
