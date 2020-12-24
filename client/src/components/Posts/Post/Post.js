4bdb5ccf5bec5bbc02d5513492f8243cb016f3c0
mani3354488 <mani3354488@gmail.com>
2020-12-24 21:40:11 +0530

    update functionality logic without redux

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -3,40 +3,42 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-
+import moment from 'moment';
 import useStyles from './style';
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
 
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
             <div className={classes.overlay}>
-                <Typography variant="h6">{moment(post.createdAt).fromNow()}</Typography>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
             <div className={classes.details}>
                 <Typography variant="body" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
-                <CardContent>
-                    <Typography className={classes.title} variant="h5" gutterBottom >{post.message}</Typography>
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                    <Button size="small" color="primary" onClick={() => }>
-                        <ThumbUpAltIcon fontsize="small" />
-                        Like
-                        {post.likeCount}
-                    </Button>
-                    <Button size="small" color="primary" onClick={() => }>
-                        <DeleteIcon fontsize="small" />
-                        Delete
-                    </Button>
-                </CardActions>
             </div>
+
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom >{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <ThumbUpAltIcon fontsize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <DeleteIcon fontsize="small" />
+                    Delete
+                </Button>
+            </CardActions>
         </Card>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
+import './index.css';
 
 // provider keep track of store which is that global state and that allows us to access that from anywhere
 
